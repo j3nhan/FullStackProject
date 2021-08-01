3fd307786be55ef45e65e2c77927a979bf18e536
j3nhan <j3nnynhan@gmail.com>
2021-08-01 15:55:55 -0700

    wusers db

--- db/migrate/20210801223843_create_users.rb
+++ db/migrate/20210801223843_create_users.rb
@@ -1,15 +1,17 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.integer :id
-      t.string :first_name
-      t.string :email
-      t.string :password_digest
-      t.string :session_token
-      t.datetime :created_at
-      t.datetime :updated_at
+      t.string :first_name, null: false
+      t.string :email, null: false
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
+      t.datetime :created_at, null: false
+      t.datetime :updated_at, null: false
 
       t.timestamps
     end
+    
+    add_index :users, :email, unique: true
+    add_index :users, :session_token, unique: true
   end
 end
