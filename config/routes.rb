0bc61780a9d99e0d033fb986c92c2fcf532ddca5
j3nhan <j3nnynhan@gmail.com>
2021-08-01 21:20:22 -0700

    worked on routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: {format: :json} do
+    resource :session, only: [:create, :destroy]
+    resources :users, only: [:index, :create, :show]
+    resources :categories, only: [:show]
+    resources :items, only: [:index, :show]
+    resources :cart_items
+    resources :reviews 
+  end
+
+  root "static_pages#root"
 end
