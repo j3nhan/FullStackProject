df86783545c75148c256a2378755242a0236d63d
j3nhan <j3nnynhan@gmail.com>
2021-08-03 09:40:53 -0700

    worked on entry file

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,23 +1,24 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import Root from './components/root';
-// import configureStore from './store/store';
-// import { signupUser, loginUser, logoutUser} from './actions/session_actions'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from './components/root';
+import configureStore from './store/store';
+import { signupUser, loginUser, logoutUser} from './actions/session_actions'
 
-// document.addEventListener('DOMContentLoaded', () => {
-//     let store;
-//     if (window.currentUser) {
-//         const preloadedState = {
-//         session: { id: window.currentUser.id },
-//         entities: {
-//             users: { [window.currentUser.id]: window.currentUser }
-//         }
-//         };
-//         store = configureStore(preloadedState);
-//         delete window.currentUser;
-//     } else {
-//         store = configureStore();
-//     }
-//     const root = document.getElementById('root');
-//     ReactDOM.render(<Root store={store} />, root);
-// });
+document.addEventListener('DOMContentLoaded', () => {
+    let store;
+    let preloadedState = undefined;
+    if (window.currentUser) {
+        preloadedState = {
+            session: { id: window.currentUser.id },
+        entities: {
+            users: { [window.currentUser.id]: window.currentUser }
+        }
+        };
+        store = configureStore(preloadedState);
+        delete window.currentUser;
+    } else {
+        store = configureStore();
+    }
+    const root = document.getElementById('root');
+    ReactDOM.render(<Root store={store} />, root);
+});
