ea34a85a2ab17b74dbaa0d95b0119e555403bb11
j3nhan <j3nnynhan@gmail.com>
2021-08-04 14:06:25 -0700

    worked on home page route

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import HomepageContainer from './home/homepage_container';
 import { Route, Switch, Link, Redirect, HashRouter } from 'react-router-dom';
+import HomepageContainer from './home/homepage_container';
 import SignupFormContainer from './register/signup_form_container';
 import SigninFormContainer from './register/signin_form_container';
 
 const App = () => (
     <div>
         <header>
-            <HomepageContainer />
+            
         </header>
 
         <Switch>
             <Route path="/signup" component={SignupFormContainer} />
             <Route path="/signin" component={SigninFormContainer} />
+            <Route path="/" component={HomepageContainer} />
         </Switch>
 
     </div>
