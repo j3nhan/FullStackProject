7c3c9f0007b2ca039b613933aeb06b4c66ffa384
j3nhan <j3nnynhan@gmail.com>
2021-08-06 23:06:22 -0700

    finished reducer

--- frontend/reducers/items_reducer.js
+++ frontend/reducers/items_reducer.js
@@ -1,11 +1,18 @@
-import { RECEIVE_ITEM } from '../actions/item_actions'
+import { RECEIVE_ITEMS, RECEIVE_ITEM } from '../actions/item_actions'
 
 const itemsReducer = (state = {}, action) => {
-    Object.freeze(state.items);
+    Object.freeze(state);
     let nextState = Object.assign({}, state);
 
     switch (action.type) {
+        case RECEIVE_ITEMS:
+            return action.items;
         case RECEIVE_ITEM:
-            
+            nextState[action.item.id] = action.item;
+            return nextState;
+        default: 
+            return state;
     }
 }
+
+export default itemsReducer;
