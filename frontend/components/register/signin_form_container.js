41c94bf1fab3207028206c7495b793ca793fd695
j3nhan <j3nnynhan@gmail.com>
2021-08-09 14:18:23 -0700

    modified the frontend folders

--- frontend/components/register/signin_form_container.js
+++ frontend/components/register/signin_form_container.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { signinUser } from '../../actions/session_actions';
-import SessionForm from './Session_form';
+// import React from 'react';
+// import { connect } from 'react-redux';
+// import { signinUser } from '../../actions/session_actions';
+// import SessionForm from './Session_form';
 
-const mapStateToProps = ({errors}) => {
-  return {
-    errors: errors.session,
-    formType: 'signin',
-  };
-};
+// const mapStateToProps = ({errors}) => {
+//   return {
+//     errors: errors.session,
+//     formType: 'signin',
+//   };
+// };
 
-const mapDispatchToProps = dispatch => {
-  return {
-    processForm: (user) => dispatch(signinUser(user)),
-  };
-};
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     processForm: (user) => dispatch(signinUser(user)),
+//   };
+// };
 
-export default connect(mapStateToProps, mapDispatchToProps)(SessionForm);
+// export default connect(mapStateToProps, mapDispatchToProps)(SessionForm);
