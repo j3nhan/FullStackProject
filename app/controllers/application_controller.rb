e5c936bde59597e1d1dc689b6affaa8d595b3ab4
j3nhan <j3nnynhan@gmail.com>
2021-08-11 12:11:19 -0700

    added camelcase in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,4 +37,25 @@ class ApplicationController < ActionController::Base
     # Prevent signed-in users from seeing certain pages
         redirect_to user_url(current_user) if signed_in?
     end
+
+    
+    before_action :underscore_params!
+
+    def underscore_params!
+        underscore_hash = -> (hash) do
+        hash.transform_keys!(&:underscore)
+        hash.each do |key, value|
+            if value.is_a?(ActionController::Parameters)
+            underscore_hash.call(value)
+            elsif value.is_a?(Array)
+            value.each do |item|
+                next unless item.is_a?(ActionController::Parameters)
+                underscore_hash.call(item)
+            end
+            end
+        end
+        end
+        underscore_hash.call(params)
+    end
+    
 end
