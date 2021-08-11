a9b54b9fa47ccda1608aabdfafa3a9b09cfdcc34
j3nhan <j3nnynhan@gmail.com>
2021-08-10 23:47:00 -0700

    cleaned up the app

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,7 +1,10 @@
-// import { Route, Switch, Link, Redirect, HashRouter } from 'react-router-dom';
-// import HomepageContainer from './home/homepage_container';
-// import SignupFormContainer from './register/signup_form_container';
+import React from 'react';
+import { Route, Switch, Link, Redirect, HashRouter } from 'react-router-dom';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
+import SignupContainer from '../signup/Signup_container';
+import SigninContainer from './signin/Signin_container';
+// import HomepageContainer from './home/homepage_container';
 
 // const App = () => (
     //     <div>
@@ -22,35 +25,18 @@
     // export default App;
     
     // ----------------------
-    import React from 'react';
-    import { Switch, BrowserRouter, Route } from "react-router-dom";
+    
     import Header from "./header/Header"
     import Homepage from "./home/Homepage"
     
-    import Item from './item/Item';
-    // import Signup from './signup/signup';
-    // import Signin from './signin/signin';
-    // import ItemShow from './item/Item_show';
-    import SigninContainer from './signin/Signin_container';
-    import { AuthRoute, ProtectedRoute } from '../util/route_util';
-    import ItemShowContainer from './item/item_show_container'
+    
+    // import ItemShowContainer from './item/item_show_container'
     
     function App() {
-        // let dispatch = useDispatch();
-        // useEffect(() => {
-    //     auth.onAuthStateChanged((authUser) => {
-    //         if (authUser) {
-    //         dispatch(setuser(authUser));
-    //     } else {
-    //         dispatch(setuser(null));
-    //     }
-    //     });
-    // }, [dispatch]);
-
     return (
-        // <BrowserRouter>
             <div>
                 <Switch>
+                    <AuthRoute exact path="/signup" component={SignupContainer} />
                     <AuthRoute exact path="/signin" component={SigninContainer} />
                     <Route exact path="/items/:itemId" component={ItemShowContainer} />
 
@@ -64,7 +50,6 @@
 
                 </Switch>
             </div>
-        // </BrowserRouter>
     )
 }
 export default App;
