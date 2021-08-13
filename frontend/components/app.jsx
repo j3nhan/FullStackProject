789ecd974c0d99d3be561a64f764481be5f594e5
j3nhan <j3nnynhan@gmail.com>
2021-08-13 10:59:33 -0700

    app edit

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Switch, Link, Redirect } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
-import SignupContainer from './signup/signup_container';
+// import SignupContainer from './signup/signup_container';
 import SigninContainer from './signin/signin_container';
 import ItemShowContainer from './item/item_show_container'
 import HomepageContainer from './home/homepage_container'
@@ -12,7 +12,7 @@ function App() {
         <div>
             <Switch>
                 <Route exact path="/" component={HomepageContainer} />
-                <AuthRoute exact path="/signup" component={SignupContainer} />
+                {/* <AuthRoute exact path="/signup" component={SignupContainer} /> */}
                 <AuthRoute exact path="/signin" component={SigninContainer} />
                 <Route exact path="/items/:itemId" component={ItemShowContainer} />
                 <Route path="*" component={NoPageFound} />
