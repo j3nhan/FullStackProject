dda7b6fa3cef06d18d040839378169896762620b
j3nhan <j3nnynhan@gmail.com>
2021-08-14 00:53:31 -0700

    worked on cart items

--- app/controllers/api/cart_items_controller.rb
+++ app/controllers/api/cart_items_controller.rb
@@ -1,28 +1,64 @@
-# class Api::CartItemsController < ApplicationController
-#     def index
-#         @cart_items = current_user.cart_items
-#     end
+class Api::CartItemsController < ApplicationController
+    def index
+        return nil if !current_user
+        @cart_items = CartItem.all.select { |cart_item| cart_item.user_id == current_user.id }
+        render :show
+    end
 
-#     def show
-#         @cart_item = CartItem.find(params[:id])
-#     end
+    def show
+        return nil if !current_user
+        @cart_item = CartItem.find(params[:id])
+    end
 
-#     def create 
-#         @cart_item = CartItem.new(cart_item_params)
-#         if @cart_item.save 
-#             render :show
-#         else 
-#             render :json['Item is not available.'], status: 422
-#         end
-#     end
+    def create 
+        if current_user && current_user.cart_items.any? {|cart_item| cart_item.item_id == params[:cart_item][:item_id].to_i}
+            @cart_item = current_user.cart_items.find_by(item_id: params[:cart_item][:item_id].to_i)
+            new_qty = @cart_item.quantity + params[:cart_item][:quantity].to_i
 
-#     def destroy 
-#         @cart_item = CartItem.find(params[:id])
-#         @cart_item.destroy 
-#         render :show
-#     end
+            if @cart_item.update_attributes({user_id: params[:cart_item][:user_id], item_id: params[:cart_item][:item_id], quantity: new_qty, fulfilled: false})
+                    @cart_items = CartItem.all.select {|cart_item| cart_item.user_id == current_user.id}        
+                    render :show
+                else
+                    render json: ['Item is not available.'], status: 401
+                end
+            else
+                @cart_item = CartItem.new(cart_item_params)
 
-#     def cart_item_params 
-#         params.require(:cartItem).permit(:user_id, :item_id, :quantity)
-#     end
-# end
+                if @cart_item.save!
+                    @cart_items = CartItem.all.select {|cart_item| cart_item.user_id == current_user.id}        
+                    render :show
+                end
+            end
+        end
+    end
+
+
+    def update 
+        @cart_item = CartItem.find_by(id: params[:id])
+        if @cart_item.user_id == current_user.id
+            if @cart_item.update_attributes(cart_item_params)
+                @cart_items = CartItem.all.select {|cart_item| cart_item.user_id == current_user.id}        
+                render :show
+            else
+                render json: ['Cart is not updated.'], status: 401
+            end
+        end
+    end
+
+    def destroy 
+        @cart_item = CartItem.find(params[:id])
+        @cart_item.destroy 
+        if current_user && @cart_item && (@cart_item.user_id == current_user.id)
+            if @cart_item.delete
+                @cart_items = CartItem.all.select {|cart_item| cart_item.user_id == current_user.id}        
+                render :show
+            end
+        else  
+            render json: ['Item is not deleted from cart.'], status: 401
+        end
+    end
+
+    def cart_item_params 
+        params.require(:cart_item).permit(:user_id, :item_id, :quantity)
+    end
+end
--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Route, Switch, Link, Redirect } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
-// import SignupContainer from './signup/signup_container';
-import SigninContainer from './signin/signin_container';
-import ItemShowContainer from './item/item_show_container'
-import HomepageContainer from './home/homepage_container'
+// import SignupContainer from './signup/Signup_container';
+import SigninContainer from './signin/Signin_container';
+import ItemShowContainer from './item/Item_show_container'
+import HomepageContainer from './home/Homepage_container'
 import NoPageFound from './No_page_found';
     
 const App = () => {
@@ -15,7 +15,7 @@ const App = () => {
                 {/* <AuthRoute exact path="/signup" component={SignupContainer} /> */}
                 <AuthRoute exact path="/signin" component={SigninContainer} />
                 <Route exact path="/items/:itemId" component={ItemShowContainer} />
-                <Route path="*" component={NoPageFound} />
+                <Route path="*" component={ NoPageFound } />
             </Switch>
         </div>
     )
