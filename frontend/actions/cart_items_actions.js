3c960bed59d50131f6c3a028346db8bdb4489861
j3nhan <j3nnynhan@gmail.com>
2021-08-14 12:41:19 -0700

    working on cart items

--- frontend/actions/cart_items_actions.js
+++ frontend/actions/cart_items_actions.js
@@ -1,54 +1,54 @@
-// import * as CartItemsAPIUtil from '../util/cart_items_api_util';
+import * as CartItemsAPIUtil from '../util/cart_items_api_util';
 
-// export const RECEIVE_CART_ITEM = 'RECEIVE_CART_ITEM';
-// export const RECEIVE_CART_ITEMS = 'RECEIVE_CART_ITEMS';
-// export const REMOVE_CART_ITEM = 'REMOVE_CART_ITEM';
+export const RECEIVE_CART_ITEM = 'RECEIVE_CART_ITEM';
+export const RECEIVE_CART_ITEMS = 'RECEIVE_CART_ITEMS';
+export const REMOVE_CART_ITEM = 'REMOVE_CART_ITEM';
 
-// const receiveCartItems = cartItems => {
-//     return {
-//         type: RECEIVE_CART_ITEMS,
-//         cartItems
-//     }
-// }
+const receiveCartItems = cartItems => {
+    return {
+        type: RECEIVE_CART_ITEMS,
+        cartItems
+    }
+}
 
-// const recieveCartItem = cartItem => {
-//     return {
-//         type: RECEIVE_CART_ITEM,
-//         cartItem
-//     }
-// }
+const recieveCartItem = cartItem => {
+    return {
+        type: RECEIVE_CART_ITEM,
+        cartItem
+    }
+}
 
-// const removeCartItem = cartItemId => {
-//     return {
-//         type: REMOVE_CART_ITEM,
-//         cartItemId: cartItemId.id
-//     }
-// }
+const removeCartItem = cartItemId => {
+    return {
+        type: REMOVE_CART_ITEM,
+        cartItemId: cartItemId.id
+    }
+}
 
-// export const fetchCartItems = () => dispatch => {
-//     return (
-//         CartItemsAPIUtil.fetchCartItems()
-//             .then(cartItems => dispatch(receiveCartItems(cartItems)))
-//     )
-// }
+export const fetchCartItems = () => dispatch => {
+    return (
+        CartItemsAPIUtil.fetchCartItems()
+            .then(cartItems => dispatch(receiveCartItems(cartItems)))
+    )
+}
 
-// export const fetchCartItem = cartItemId => dispatch => {
-//     return (
-//         CartItemsAPIUtil.fetchCartItem(cartItemId)
-//             .then(cartItem => dispatch(receiveCartItem(cartItem)))
-//     )
-// }
+export const fetchCartItem = cartItemId => dispatch => {
+    return (
+        CartItemsAPIUtil.fetchCartItem(cartItemId)
+            .then(cartItem => dispatch(receiveCartItem(cartItem)))
+    )
+}
 
-// export const createCartItem = cartItem => dispatch => {
-//     return (
-//         CartItemsAPIUtil.createCartItem(cartItem)
-//             .then(cartItem => dispatch(recieveCartItem(cartItem)))
-//     )
-// }
+export const createCartItem = cartItem => dispatch => {
+    return (
+        CartItemsAPIUtil.createCartItem(cartItem)
+            .then(cartItem => dispatch(recieveCartItem(cartItem)))
+    )
+}
 
-// export const deleteCartItem = cartItemId => dispatch => {
-//     return (
-//         CartItemsAPIUtil.deleteCartItem(cartItemId)
-//             .then((cartItemId) => dispatch(removeCartItem(cartItemId)))
-//     )
-// }
+export const deleteCartItem = cartItemId => dispatch => {
+    return (
+        CartItemsAPIUtil.deleteCartItem(cartItemId)
+            .then((cartItemId) => dispatch(removeCartItem(cartItemId)))
+    )
+}
