9215f861b9368967af752f8fca396a08d9f1e92a
j3nhan <j3nnynhan@gmail.com>
2021-08-17 20:21:36 -0700

    changed items controller

--- app/controllers/api/cart_items_controller.rb
+++ app/controllers/api/cart_items_controller.rb
@@ -1,51 +1,42 @@
 class Api::CartItemsController < ApplicationController
-    def index
-        return nil if !current_user
-        @cart_items = CartItem.all.select { |cart_item| cart_item.user_id == current_user.id }
-        render :show
-    end
-
     def show
-        return nil if !current_user
         @cart_item = CartItem.find(params[:id])
+        return nil if !current_user
+
+        if @cart_item
+            render :show
+        else
+            render json: @cart_item.errors.full_messages, status: 404 
+        end
     end
 
     def create
-        @cart_item = CartItem.new(cart_item_params)
-        if @item.save
+        cart_item_params[:user_id] != "" ? @cart_item = CartItem.new(cart_item_params) : @cart_item = CartItem.new()
+
+        if @cart_item.save!
             render :show
-        else 
-            render json: @item.errors.full_messages, status: 422
+        else
+            render json: @cart_item.errors.full_messages, status: 401
         end
     end
 
     def update 
         @cart_item = CartItem.find_by(id: params[:id])
-        if @cart_item.user_id == current_user.id
-            if @cart_item.update_attributes(cart_item_params)
-                @cart_items = CartItem.all.select {|cart_item| cart_item.user_id == current_user.id}        
-                render :show
-            else
-                render json: ['Cart is not updated.'], status: 401
-            end
+        if @cart_item
+            @cart_item.update(cart_item_params)
+            render :show
+        else
+            render json: ['Cart is not updated.'], status: 401
         end
     end
 
     def destroy 
         @cart_item = CartItem.find(params[:id])
         @cart_item.destroy 
-        if current_user && @cart_item && (@cart_item.user_id == current_user.id)
-            if @cart_item.delete
-                @cart_items = CartItem.all.select {|cart_item| cart_item.user_id == current_user.id}        
-                render :show
-            end
-        else  
-            render json: ['Item is not deleted from cart.'], status: 401
-        end
     end
 
     def cart_item_params 
-        params.require(:cart_item).permit(:user_id, :item_id, :quantity)
+        params.require(:cart_item).permit(:id, :item_id, :user_id)
     end
 
 end
