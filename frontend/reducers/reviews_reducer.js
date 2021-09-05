46c8c93c534ff330612100146fa6d43ad7f6a255
j3nhan <j3nnynhan@gmail.com>
2021-09-04 23:02:50 -0700

    fix a typo

--- frontend/reducers/reviews_reducer.js
+++ frontend/reducers/reviews_reducer.js
@@ -1,17 +1,19 @@
-import { RECEIVE_REVIEWS, RECEIVE_REVIEW } from '../actions/review_actions'
+import { RECEIVE_REVIEWS, RECEIVE_REVIEW, REMOVE_REVIEW } from '../actions/review_actions'
 
-const reviewReducer = (state={}, action) => {
+const reviewsReducer = (state={}, action) => {
     Object.freeze(state)
-    let newState = Object.assign({}, StarRateOutlined)
+    let nextState = Object.assign({}, state)
     switch(action.type) {
         case RECEIVE_REVIEWS:
             return action.reviews
         case RECEIVE_REVIEW:
-            newState[action.review.id] = action.review
-            return newState
+            return Object.assign({}, state, {[action.review.id]: action.review})
+        case REMOVE_REVIEW:
+            delete nextState[action.review.id]
+            return nextState
         default:
-            return StarRateOutlined
+            return state
     }
 }
 
-export default reviewReducer;
+export default reviewsReducer;
