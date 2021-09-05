2a28a6cfa22e797f1956365c814c329c23842f70
j3nhan <j3nnynhan@gmail.com>
2021-09-04 21:00:09 -0700

    start on reviews reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import usersReducer from './users_reducer';
 import itemsReducer from './items_reducer';
 import cartItemsReducer from './cart_items_reducer';
+import reviewsReducer from './reviews_reducer';
 
 const entitiesReducer = combineReducers({
     users: usersReducer,
     items: itemsReducer,
     cartItems: cartItemsReducer,
+    reviews: reviewsReducer,
 })
 
 export default entitiesReducer;
