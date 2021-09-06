abc6e284dc60246929b6173e75b27fc4808cd0ac
j3nhan <j3nnynhan@gmail.com>
2021-09-06 14:08:21 -0700

    change the review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,8 +2,8 @@ class Review < ApplicationRecord
     validates :title, :body, :rating, :item_id, :author, :author_id, presence: true
 
     belongs_to :user,
-        foreign_key: :author_id,
+        foreign_key: :author_id
 
     belongs_to :item,
-        foreign_key: :item_id,
+        foreign_key: :item_id
 end
