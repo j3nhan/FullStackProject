03421d063fbbe108b913dddef742901098d50b09
j3nhan <j3nnynhan@gmail.com>
2021-09-07 12:28:26 -0700

    change the model

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,6 +1,6 @@
 class CartItem < ApplicationRecord
-    validates :user_id, presence: true
+    validates :user_id
 
     belongs_to :users, foreign_key: :user_id
-    belongs_to :items, foreign_key: :item_id
+    has_many :items, foreign_key: :item_id
 end
