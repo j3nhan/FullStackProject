dc718d455a6b4472e34040a8f5c83985cca35399
j3nhan <j3nnynhan@gmail.com>
2021-09-07 15:05:56 -0700

    belongs to items

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,4 @@
 class CartItem < ApplicationRecord
     belongs_to :users, foreign_key: :user_id, optional: true
-    has_many :items, foreign_key: :item_id
+    belongs_to :items, foreign_key: :item_id, optional: true
 end
