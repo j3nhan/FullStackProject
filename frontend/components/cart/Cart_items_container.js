edc7eda33b81cda30842324a8dde792923f0d1b1
j3nhan <j3nnynhan@gmail.com>
2021-09-07 15:30:06 -0700

    added cart item

--- frontend/components/cart/Cart_items_container.js
+++ frontend/components/cart/Cart_items_container.js
@@ -1,18 +1,19 @@
 import { connect } from 'react-redux';
 import CartItems from './Cart_items';
-import { fetchCartItem, clearCart } from '../../actions/cart_items_actions';
+import { fetchCartItems, fetchCartItem, createCartItem, deleteCartItem, clearCart } from '../../actions/cart_items_actions';
 import { deleteItem } from '../../actions/item_actions';
 
 const mapStateToProps = (state, ownProps) => ({
     currentUser: state.entities.users[state.session.id],
     cartItems: state.entities.cartItems,
-    items: state.entities.cartItems.items
 });
 
 
 const mapDispatchToProps = dispatch => ({
+    fetchCartItems: () => dispatch(fetchCartItems()),
     fetchCartItem: cartItemId => dispatch(fetchCartItem(cartItemId)),
-    deleteItem: itemId => dispatch(deleteItem(itemId)),
+    createCartItem: cartItem => dispatch(createCartItem(cartItem)),
+    deleteCartItem: cartItemId => dispatch(deleteCartItem(cartItemId)),
     clearCart: () => dispatch(clearCart())
 });
 
