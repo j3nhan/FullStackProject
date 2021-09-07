e631b1b3266b167efde7f67df0388092bf7a8d96
j3nhan <j3nnynhan@gmail.com>
2021-09-07 15:05:33 -0700

    change a typo

--- app/controllers/api/cart_items_controller.rb
+++ app/controllers/api/cart_items_controller.rb
@@ -33,10 +33,11 @@ class Api::CartItemsController < ApplicationController
     def destroy 
         @cart_item = CartItem.find(params[:id])
         @cart_item.destroy 
+        render :show
     end
 
     def cart_item_params 
-        params.require(:cart_item).permit(:id, :item_id, :user_id)
+        params.require(:cart_item).permit(:id, :user_id, :item_id)
     end
 
 end
