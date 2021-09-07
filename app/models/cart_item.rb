656dd0df87e5223bc8e7349ca8e90b71bf1e0ec0
j3nhan <j3nnynhan@gmail.com>
2021-09-07 12:50:23 -0700

    add cart items

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,6 +1,4 @@
 class CartItem < ApplicationRecord
-    validates :user_id
-
-    belongs_to :users, foreign_key: :user_id
+    belongs_to :users, foreign_key: :user_id, optional: true
     has_many :items, foreign_key: :item_id
 end
