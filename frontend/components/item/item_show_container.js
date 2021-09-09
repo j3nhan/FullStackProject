74fb7fe40d58e3a58400ea7c525a62b3d92f6b85
j3nhan <j3nnynhan@gmail.com>
2021-09-09 11:57:54 -0700

    item show container needs to be modified

--- frontend/components/item/item_show_container.js
+++ frontend/components/item/item_show_container.js
@@ -5,13 +5,14 @@ import ItemShow from './Item_show';
 
 const mapStateToProps = (state, ownProps) => ({
     item: state.entities.items[ownProps.match.params.itemId],
-    itemAdded: state.entities.cartItems,
+    itemIdMat: ownProps.match.params.itemId,
+    itemsAdded: Object.values(state.entities.cartItems),
     currentUser: state.session.id,
 })
 
 const mapDispatchToProps = dispatch => ({
-    fetchItems: () => dispatch(fetchItems()),
-    fetchItem: itemId => dispatch(fetchItem(itemId)),
+    fetchItems: items => dispatch(fetchItems(items)),
+    fetchItem: itemIdMat => dispatch(fetchItem(itemIdMat)),
     addCartItem: item => dispatch(createCartItem(item))
 })
 
