fef0c59a4f48a126b2b70b159862771e2990f175
j3nhan <j3nnynhan@gmail.com>
2021-09-09 11:58:21 -0700

    reducer is not working

--- frontend/reducers/cart_items_reducer.js
+++ frontend/reducers/cart_items_reducer.js
@@ -1,13 +1,10 @@
 import { RECEIVE_CART_ITEM, RECEIVE_CART_ITEMS, REMOVE_CART_ITEM, CLEAR_CART } from "../actions/cart_items_actions";
-import { RECEIVE_CURRENT_USER } from "../actions/session_actions";
 
 const cartItemsReducer = (state = {}, action) => {
     Object.freeze(state);
     let nextState = Object.assign({}, state);
 
     switch (action.type) {
-        case RECEIVE_CURRENT_USER: 
-            return Object.assign({}, state, action.user.cartItems);
         case RECEIVE_CART_ITEMS:
             return action.cartItems;
         case RECEIVE_CART_ITEM:
@@ -17,7 +14,8 @@ const cartItemsReducer = (state = {}, action) => {
             delete nextState[action.cartItemId];
             return nextState;
         case CLEAR_CART:
-            return {...state, cart: []};
+            // return {...state, cart: []};
+            return {};
         default: 
             return state;
     }
