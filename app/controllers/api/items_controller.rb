855aa37bc764a977fdc155c1a6ca3fa9415bd55b
j3nhan <j3nnynhan@gmail.com>
2021-09-10 16:08:32 -0700

    edit items controller

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -8,32 +8,5 @@ class Api::ItemsController < ApplicationController
         @item = Item.find(params[:id])
         render :show
     end
-
-    def update
-        @item = Item.find(params[:id])
-        if @item
-            @item.update(item_params)
-            render :show
-        end
-    end
-
-    def create
-        @item = Item.new(item_params)
-        if @item.save!
-            render :show
-        else
-            render json: @item.errors.full_messages, status: 401
-        end
-    end
-
-    def destroy
-        @item = Item.find(params[:id])
-        @item.destroy
-    end
-
-    private
-    def item_params
-        params.require(:item).permit(:item_name, :description, :price, :rating, :cart_item_id, photos: [])
-    end
-
+    
 end
