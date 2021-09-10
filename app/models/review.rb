bba95a406fc8c55f6891dbbbdff41f356cbf48fa
j3nhan <j3nnynhan@gmail.com>
2021-09-10 16:46:41 -0700

    write review

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,5 @@ class Review < ApplicationRecord
     validates :title, :body, :rating, :item_id, :author, :author_id, presence: true
 
     belongs_to :user, foreign_key: :author_id
-
     belongs_to :item, foreign_key: :item_id
 end
