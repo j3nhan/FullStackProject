e5b15ad004c74b087675f994b5f7ecfbf90637f5
j3nhan <j3nnynhan@gmail.com>
2021-09-10 10:26:52 -0700

    made changes to cart container

--- frontend/components/cart/Cart_container.js
+++ frontend/components/cart/Cart_container.js
@@ -1,22 +1,22 @@
-import { withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
 import Cart from './Cart'
-import { fetchCartItems, updateCartItem, deleteCartItem} from '../../actions/cart_items_actions'
+import { fetchCartItems, updateCartItem, deleteCartItem, clearCart} from '../../actions/cart_items_actions'
 
-const mSTP = state => {
+const mapStateToProps = state => {
     return ({
-        currentUser: state.users.id,
+        currentUser: state.session.id,
         cartItems: state.entities.cartItems,
         cartItemId: Object.keys(state.entities.cartItems)
     })
 }
 
-const mDTP = dispatch => {
+const mapDispatchToProps = dispatch => {
     return {
         fetchCartItems: () => dispatch(fetchCartItems()),
         updateCartItem: cartItem => dispatch(updateCartItem(cartItem)),
         deleteCartItem: cartItemId => dispatch(deleteCartItem(cartItemId)),
+        clearCart: () => dispatch(clearCart())
     }
 }
 
-export default withRouter(connect(mSTP, mDTP)(Cart))
+export default connect(mapStateToProps, mapDispatchToProps)(Cart);
