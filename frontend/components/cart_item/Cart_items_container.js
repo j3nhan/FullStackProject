8ccec20c4452db04ed4aece2bda9b1e07a92f3e1
j3nhan <j3nnynhan@gmail.com>
2021-09-10 16:47:54 -0700

    include update and delete cart item props

--- frontend/components/cart_item/Cart_items_container.js
+++ frontend/components/cart_item/Cart_items_container.js
@@ -1,10 +1,11 @@
 import { connect } from 'react-redux';
 import CartItems from './Cart_items';
-import { fetchCartItems, fetchCartItem, createCartItem, deleteCartItem, clearCart } from '../../actions/cart_items_actions';
+import { fetchCartItems, fetchCartItem, createCartItem, updateCartItem, deleteCartItem, clearCart } from '../../actions/cart_items_actions';
 
 const mapStateToProps = state => ({
     currentUser: state.session.id,
     currCartItems: state.entities.cartItems,
+    cartItemId: Object.keys(state.entities.cartItems)
 });
 
 
@@ -12,6 +13,7 @@ const mapDispatchToProps = dispatch => ({
     fetchCartItems: () => dispatch(fetchCartItems()),
     fetchCartItem: cartItemId => dispatch(fetchCartItem(cartItemId)),
     createCartItem: cartItem => dispatch(createCartItem(cartItem)),
+    updateCartItem: cartItem => dispatch(updateCartItem(cartItem)),
     deleteCartItem: cartItemId => dispatch(deleteCartItem(cartItemId)),
     clearCart: () => dispatch(clearCart())
 });
