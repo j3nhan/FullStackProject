cd8885c7da5eb9f968b927d655516c122d814264
j3nhan <j3nnynhan@gmail.com>
2021-09-11 14:39:38 -0700

    work on the item show

--- frontend/components/item/Item_show.jsx
+++ frontend/components/item/Item_show.jsx
@@ -7,6 +7,7 @@ import Header from '../header/Header';
 class ItemShow extends React.Component {
     componentDidMount() {
         this.props.fetchItem(this.props.itemIdMat);
+        this.addToCart = this.addToCart.bind(this);
     }
     
     componentDidUpdate(prevProps) {
@@ -15,18 +16,19 @@ class ItemShow extends React.Component {
         }
     }
 
-    addToCart = e => {
-        e.preventDefault();
-        this.props.addCartItem({
-            user_id: this.props.currentUser,
-            item_id: this.props.itemIdMat,
-            quantity: 1
-        })
-    }
-
-    updateQuantity = e => {
+    addToCart(e) {
         e.preventDefault();
 
+        if (this.props.currentUser) {
+            this.props.createCartItem({
+                user_id: this.props.currentUser,
+                item_id: this.props.itemIdMat,
+                quantity: 1
+            })
+            this.props.history.push('/checkout')
+        } else {
+            this.props.history.push('/signin')
+        }
     }
 
     render() {
@@ -42,47 +44,48 @@ class ItemShow extends React.Component {
         
         if (item) return (
             <div>
-            <Header/>
-            <div className='itemshow-div'>
-                <div className='itemshow-main-div'>
-                    <div className='itemshow-pic-div'>
-                        <img className='itemshow-pic-img' src={item.photoUrl} />
-                    </div>
+                <Header/>
+                <div className='itemshow-div'>
+                    <div className='itemshow-main-div'>
+                        <div className='itemshow-pic-div'>
+                            <img className='itemshow-pic-img' src={item.photoUrl} />
+                        </div>
 
-                    <div className='itemshow-details-div'>
-                        <div className='itemshow-details-name'>
-                            {item.itemName}
-                            <p className='itemshow-details-seller'>Support Yours Truly</p>
-                        </div>
-                        <div className='itemshow-details-rating-div'>
-                            <div className="rating">
-                                {Array(item.rating).fill().map((_, idx) => (
-                                    <div key={ idx }>
-                                        <span role="img" aria-label="star">⭐</span>
-                                    </div>
-                                ))}
+                        <div className='itemshow-details-div'>
+                            <div className='itemshow-details-name'>
+                                {item.itemName}
+                                <p className='itemshow-details-seller'>Support Yours Truly</p>
                             </div>
+                            <div className='itemshow-details-rating-div'>
+                                <div className="rating">
+                                    {Array(item.rating).fill().map((_, idx) => (
+                                        <div key={ idx }>
+                                            <span role="img" aria-label="star">⭐</span>
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                            <div className='itemshow-details-price-div'>
+                            <p>Price: </p>
+                            <p className='itemshow-details-price'>{moneyFormatter.format(item.price / 100)}</p>
+                            </div>
+                            <div className='itemshow-details-description-div'>
+                                <p>About this item</p>
+                                <ul className='itemshow-details-description-bullets'>
+                                    <li>{item.description}</li>
+                                    <li>ValYOU's Choice</li>
+                                    <li>Environmentally Friendly and Cruelty-Free</li>
+                                    <li>Made with love and care</li>
+                                    <li>Fast and Free Shipping</li>
+                                    <li>Eligible for Return, Refund or Replacement</li>
+                                </ul>
+                            </div>
+                            
+                            <button className="clickbutton" onClick={this.addToCart}>Add to Cart</button>
                         </div>
-                        <div className='itemshow-details-price-div'>
-                        <p>Price: </p>
-                        <p className='itemshow-details-price'>{moneyFormatter.format(item.price / 100)}</p>
-                        </div>
-                        <div className='itemshow-details-description-div'>
-                            <p>About this item</p>
-                            <ul className='itemshow-details-description-bullets'>
-                                <li>{item.description}</li>
-                                <li>ValYOU's Choice</li>
-                                <li>Environmentally Friendly and Cruelty-Free</li>
-                                <li>Made with love and care</li>
-                                <li>Fast and Free Shipping</li>
-                                <li>Eligible for Return, Refund or Replacement</li>
-                            </ul>
-                        </div>
-                        <button className="clickbutton" onClick={this.addToCart}>Add to Cart</button>
                     </div>
                 </div>
             </div>
-        </div>
         )
 
         return null
