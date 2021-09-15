39e0a8066261113c6e4e2a705da2bde2af3fb702
j3nhan <j3nnynhan@gmail.com>
2021-09-15 13:57:13 -0700

    change controller

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -18,12 +18,17 @@ class Api::ReviewsController < ApplicationController
         @review = Review.new(review_params)
         @review.author_id = current_user.id
         @review.item_id = params[:review][:item_id]
+
         if @review.save!
             @reviews = Review.all
             render :index
         else
             render json: @review.errors.full_messages, status: 401
         end
+    end
+
+    def destroy
+        @review = current_user.authored_reviews.find(params[:id]).destroy
     end
 
     def review_params
