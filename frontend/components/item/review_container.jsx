eaf94a58351428067cd2e047b68625442a82eeb2
j3nhan <j3nnynhan@gmail.com>
2021-09-15 16:34:47 -0700

    review container in progress

---
+++ frontend/components/item/review_container.jsx
@@ -0,0 +1,23 @@
+import { connect } from 'react-redux';
+import Review from './Review';
+import { createReview } from '../../actions/review_actions';
+import { withRouter } from 'react-router-dom';
+
+const mapStateToProps = (state, ownProps) => ({
+        formType: "Create Review",
+        review: {
+            user_id: state.session.id,
+            title: '',
+            body: '',
+            item_id: ownProps.match.params.itemId,
+            rating: 1
+        },
+        item: state.entities.items[ownProps.match.params.itemId]
+
+});
+
+const mapDispatchToProps = dispatch => ({
+    createReview: (review, itemId) => dispatch(createReview(review, itemId))
+});
+
+export default withRouter(connect(mapStateToProps, mapStateToProps)(Review));
