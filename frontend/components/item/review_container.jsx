fc2e5cb41e0c8cc499431317ffe18060a50450d2
j3nhan <j3nnynhan@gmail.com>
2021-09-16 16:25:54 -0700

    work on review container

--- frontend/components/item/review_container.jsx
+++ frontend/components/item/review_container.jsx
@@ -1,22 +1,21 @@
 import { connect } from 'react-redux';
 import Review from './Review';
 import { createReview } from '../../actions/review_actions';
-import { withRouter } from 'react-router-dom';
 
 const mapStateToProps = (state, ownProps) => ({
+    item: state.entities.items[ownProps.match.params.itemId],
+
     review: {
-        user_id: state.session.id,
         title: '',
         body: '',
+        rating: 1,
         item_id: ownProps.match.params.itemId,
-        rating: 1
+        author_id: state.session.id,
     },
-    
-    item: state.entities.items[ownProps.match.params.itemId]
 });
 
 const mapDispatchToProps = dispatch => ({
     createReview: (review, itemId) => dispatch(createReview(review, itemId))
 });
 
-export default withRouter(connect(mapStateToProps, mapStateToProps)(Review));
+export default connect(mapStateToProps, mapDispatchToProps)(Review);
