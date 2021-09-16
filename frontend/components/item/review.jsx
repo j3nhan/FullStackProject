0618c8d28e979528051522101c57fbffaf2c7cc9
j3nhan <j3nnynhan@gmail.com>
2021-09-15 20:21:04 -0700

    review form for item show

--- frontend/components/item/review.jsx
+++ frontend/components/item/review.jsx
@@ -1,19 +1,12 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 import Rating from 'react-rating';
 
 class Review extends React.Component {
     constructor(props) {
         super(props);
-        this.state ={
-            title: '',
-            body: '',
-            item_id: this.props.item.id,
-            author: this.props.currentUser ? this.props.currentUser.name : '', 
-            author_id: state.session.id
-        }
-
+        this.state = this.props.review
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.updateReview = this.updateReview.bind(this);
         this.updateState = this.updateState.bind(this);
     }
 
@@ -23,9 +16,63 @@ class Review extends React.Component {
         .then(() => this.props.history.push(`/items/${this.props.item.id}`))
     }
 
-    updateReview(prop) {
-
+    updateReview(field) {
+        return e => this.setState({ [field]: e })
     }
 
+    updateState(field) {
+        return e => this.setState({ [field]: e.target.value })
+    }
+
+    render() {
+        const { item } = this.props;
+
+        const starIcon = (
+            <img className="rating-star" src={'images/staricon.svg'} />
+        )
+
+        const noStar = (
+            <img className="rating-star" src={'images/nostar.svg'} />
+        )
+        
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <h3>Create Review</h3>
+                    <div>
+                        <img src={item.photoUrl} />
+                        <div>{item.itemName}</div>
+                    </div>
+
+                    <div>
+                        <h4>Overall Rating</h4>
+                        <Rating
+                            initialRating={ this.state.rating }
+                            emptySymbol={ noStar }
+                            fullSymbol={ starIcon }
+                            onChange={ this.updateReview('rating') }
+                        />
+                    </div>
+
+                    <div>
+                        <h4>Add a headline</h4>
+                        <input type="text" onChange={this.updateState('title')} value={this.state.title} placeholder="What's most important to know?"/>
+                    </div>
+
+                    <div>
+                        <h4>Add a written review</h4>
+                        <textarea onChange={this.updateState('body')} value={this.state.body} placeholder="What did you like or dislike? What did you use this product for?" ></textarea>                    
+                    </div>
+
+                    <div>
+                        <button>Submit</button>
+                    </div>
+                </form>
+            </div>
+        )
+
+    }
     
 }
+
+export default Review;
