2d07b03301deb488a15e3d1e203766e77214c9ad
j3nhan <j3nnynhan@gmail.com>
2021-09-17 22:03:53 -0700

    remove author from model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
-    validates :title, :body, :rating, :item_id, :author, :author_id, presence: true
+    validates :title, :body, :rating, :item_id, :author_id, presence: true
 
     belongs_to :user, foreign_key: :author_id
     belongs_to :item, foreign_key: :item_id
