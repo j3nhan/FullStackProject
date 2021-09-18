a6561c696516e036b01c0ee4a2e69973c987401b
j3nhan <j3nnynhan@gmail.com>
2021-09-17 22:05:08 -0700

    find typo in backend

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resource :session, only: [:create, :destroy, :show]
     resources :users, only: [:create] 
-    resources :items, only: [:index, :show]
+    resources :items, only: [:index, :show] do 
+      resources :reviews, only: [:index, :show, :create]
+    end
+    resources :reviews, only: [:destroy]
     resources :cart_items, except: [:new, :edit]
-    resources :reviews 
   end
 
   root "static_pages#root"
