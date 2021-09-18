5edac543676dd7cba46417a258bd76571b4be979
j3nhan <j3nnynhan@gmail.com>
2021-09-17 22:05:37 -0700

    remove author from reviews table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_10_191516) do
+ActiveRecord::Schema.define(version: 2021_09_18_014240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,7 +67,6 @@ ActiveRecord::Schema.define(version: 2021_09_10_191516) do
     t.text "body", null: false
     t.integer "rating", null: false
     t.integer "item_id", null: false
-    t.string "author", null: false
     t.integer "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
