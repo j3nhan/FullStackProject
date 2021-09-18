d490f91c6d93b164e630257471cae22b3a9f1ea5
j3nhan <j3nnynhan@gmail.com>
2021-09-17 22:08:40 -0700

    review component includes params

--- frontend/components/item/review.jsx
+++ frontend/components/item/review.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import Rating from 'react-rating';
 import Header from '../header/Header';
+import LoadingPage from '../Loading_page';
 
 class Review extends React.Component {
     constructor(props) {
         super(props);
+
         this.state = this.props.review
         this.handleSubmit = this.handleSubmit.bind(this);
         this.updateReview = this.updateReview.bind(this);
         this.updateState = this.updateState.bind(this);
     }
 
+    componentDidMount() {
+        this.props.fetchItem(this.props.itemIdMat);
+    }
+
     handleSubmit(e) {
         e.preventDefault();
-        this.props.createReview(this.state)
-        .then(() => this.props.history.push(`/items/${this.props.item.id}`))
+        this.props.createReview(this.state, this.props.itemIdMat)
+        .then(() => this.props.history.push(`/items/${this.props.itemIdMat}`))
     }
 
     updateReview(field) {
@@ -27,33 +33,41 @@ class Review extends React.Component {
 
     render() {
         const { item } = this.props;
+        // const starIcon = (
+        //     <img className="rating-star" src={'images/staricon.svg'} />
+        // )
 
-        const starIcon = (
-            <img className="rating-star" src={'images/staricon.svg'} />
-        )
+        // const noStar = (
+        //     <img className="rating-star" src={'images/nostar.svg'} />
+        // )
 
-        const noStar = (
-            <img className="rating-star" src={'images/nostar.svg'} />
-        )
+        if (!item) {
+            return (
+                <div>
+                    <LoadingPage/>
+                </div>
+            )
+        }
         
         return (
             <div>
+
                 <Header/>
                 <form onSubmit={this.handleSubmit}>
                     <h3>Create Review</h3>
                     <div>
-                        {/* <img src={this.props.item.photoUrl} /> */}
-                        {/* <div>{item.itemName}</div> */}
+                        <img src={this.props.item.photoUrl} /> 
+                        <div>{this.props.item.itemName}</div>
                     </div>
 
                     <div>
-                        <h4>Overall Rating</h4>
-                        <Rating
+                        {/* <h4>Overall Rating</h4> */}
+                        {/* <Rating
                             initialRating={ this.state.rating }
                             emptySymbol={ noStar }
                             fullSymbol={ starIcon }
                             onChange={ this.updateReview('rating') }
-                        />
+                        /> */}
                     </div>
 
                     <div>
