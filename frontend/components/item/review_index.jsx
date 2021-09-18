9715f5e00f32405ac7bfdc20e95155ec062d8bf0
j3nhan <j3nnynhan@gmail.com>
2021-09-17 22:09:27 -0700

    review index is displayed

--- frontend/components/item/review_index.jsx
+++ frontend/components/item/review_index.jsx
@@ -1,24 +1,26 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import Rating from 'react-rating';
 
 class ReviewIndex extends React.Component {
     constructor(props) {
         super(props);
-
         this.deleteReview = this.deleteReview.bind(this);
     }
 
     componentDidMount() {
-        this.props.fetchReviews(this.props.itemId)
+        this.props.fetchReviews(this.props.itemIdMat)
     }
 
     deleteReview(reviewId) {
-        this.props.deleteReview(reviewId)
-        this.props.history.push('/items/:itemId')
+        return (() => 
+            this.props.deleteReview(reviewId)
+            .then(() => this.props.history.push(`/items/${this.props.itemIdMat}`))
+        )
     }
 
     render() {
+        console.log(this.props);
         const { reviews } = this.props;
         const starIcon = (
             <img className="rating-star" src={'images/staricon.svg'} />
@@ -29,26 +31,25 @@ class ReviewIndex extends React.Component {
 
         return (
             <div>
-                <h1>Customer reviews</h1>
+                <h2>Top Global Reviews</h2>
                 <ul>
                     {reviews.map(review => {
                         const dateCreated = new Date(review.created_at).toDateString()
+
                         return (
                             <div key={review.id}>
                                 <div>
                                     <img src={'images/profilepic.jpeg'} alt='profile'/>
-                                    <div>
-                                        {review.author}
-                                    </div>
+                                    <div>{review.author}</div>
                                 </div>
 
                                 <div>
                                     <div>
                                         <Rating 
-                                            initialRating={review.rating}
-                                            emptySymbol={empty}
-                                            fullSymbol={full}
-                                            readonly={true}
+                                            initialRating={ review.rating }
+                                            emptySymbol={ noStar }
+                                            fullSymbol={ starIcon }
+                                            readonly={ true }
                                             className="rating-symbol"
                                         />
                                     </div>
@@ -57,27 +58,20 @@ class ReviewIndex extends React.Component {
                                 </div>
 
                                 <div>Reviewed on {dateCreated}</div>
-
                                 <div>Verified Purchase</div>
-
                                 <div>{review.body}</div>
 
                                 <div>
-                                    <button>Helpful</button>
-                                    <div>Report Abuse</div>
                                     <div>
-                                        {this.props.authorId === review.user_id ?
+                                        {this.props.currentUser === review.author_id ?
                                             <div>
-                                                <button className="review-delete-btn"
-                                                    onClick={this.deleteReview(review.id)}>Delete
-                                                </button>
+                                                <button className="review-delete-btn" onClick={this.deleteReview(review.id)}>Delete</button>
                                             </div>
-                                            : null
+                                            : 
+                                            null
                                         }
                                     </div>
-
                                 </div>
-
                             </div>
                         )
                     })}
