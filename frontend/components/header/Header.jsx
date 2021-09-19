a7634d7f7d176a35cacc8c522d2ead668e8276f0
j3nhan <j3nnynhan@gmail.com>
2021-09-19 13:04:50 -0700

    cart item is working

--- frontend/components/header/Header.jsx
+++ frontend/components/header/Header.jsx
@@ -1,63 +1,224 @@
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// import SearchIcon from '@material-ui/icons/Search';
+// import LocationOnIcon from '@material-ui/icons/LocationOn';
+// import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+
+// const categories = [
+//     "Mom & Pop",
+//     "Farmers Market",
+//     "Women-Owned",
+//     "Veteran-Owned",
+//     "Disability-Owned"
+// ]
+
+
+// const Header = () => {
+//     return (
+//         <div>
+//             <nav className="header">   
+//                 <Link to="/">
+//                     <img className="header-logo" 
+//                         src="images/valyou-logo.png"
+//                         alt="valyou-logo" 
+//                         />
+//                 </Link>
+
+//                 <div className="header-option" style={{marginRight: "-10px"}}>
+//                     <LocationOnIcon/>
+//                 </div>
+
+//                 <div className="header-option">
+//                     <span className="header-option1">Deliver to </span>
+//                     <span className="header-option2">Home Sweet Home</span>
+//                 </div>
+
+//                 <div className="search">
+//                     <select>
+//                         <option>All</option>
+//                     </select>
+//                     <input type="text" className="searchInput" />
+//                     <SearchIcon className="searchIcon" />
+//                 </div>
+
+//                 <div className="header-nav">
+//                     <Link to='/checkout' className='header-link'>
+//                         <div className="header-basket">
+//                             <ShoppingCartIcon/>
+//                             <span className="header-option2-basket basket-count">0</span>
+//                         </div>
+//                     </Link>
+//                 </div>
+//             </nav>
+
+//             <div className="item-container">
+//                 {categories && categories.map((category, idx) =>
+//                     <p key={ idx }>{category}</p>
+//                 )}
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default Header;
+
+
+
+
+
+
+
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import SearchIcon from '@material-ui/icons/Search';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 
-const categories = [
-    "Mom & Pop",
-    "Farmers Market",
-    "Women-Owned",
-    "Veteran-Owned",
-    "Disability-Owned"
-]
+class Header extends React.Component {
+    constructor(props) {
+        super(props);
 
+        this.userHomepage = this.userHomepage.bind(this);
+        this.guestHomepage = this.guestHomepage.bind(this);
+        this.signedoutHome = this.signedoutHome.bind(this);
+        this.returnHome = this.returnHome.bind(this);
+    }
+    
+    signedoutHome() {
+        this.props.signoutUser();
+        this.props.history.push('/');
+    }
 
-const Header = () => {
-    return (
-        <div>
-            <nav className="header">   
-                <Link to="/">
-                    <img className="header-logo" 
-                        src="images/valyou-logo.png"
-                        alt="valyou-logo" 
-                        />
-                </Link>
+    returnHome() {
+        let data = '';
+        this.props.fetchItems(data);
+    }
 
-                <div className="header-option" style={{marginRight: "-10px"}}>
-                    <LocationOnIcon/>
-                </div>
-
-                <div className="header-option">
-                    <span className="header-option1">Deliver to </span>
-                    <span className="header-option2">Home Sweet Home</span>
-                </div>
-
-                <div className="search">
-                    <select>
-                        <option>All</option>
-                    </select>
-                    <input type="text" className="searchInput" />
-                    <SearchIcon className="searchIcon" />
-                </div>
-
-                <div className="header-nav">
-                    <Link to='/checkout' className='header-link'>
-                        <div className="header-basket">
-                            <ShoppingCartIcon/>
-                            <span className="header-option2-basket basket-count">0</span>
-                        </div>
+    userHomepage() {
+        return (
+            <div>
+                <nav className="header">   
+                    <Link to="/">
+                        <img className="header-logo" src="images/valyou-logo.png"/>
                     </Link>
-                </div>
-            </nav>
 
-            <div className="item-container">
-                {categories && categories.map((category, idx) =>
-                    <p key={ idx }>{category}</p>
-                )}
+                    <div className="header-option" style={{marginRight: "-10px"}}>
+                        <LocationOnIcon/>
+                    </div>
+
+                    <div className="header-option">
+                        <span className="header-option1">Deliver to {this.props.currentUser.name}'s</span>
+                        <span className="header-option2">Home Sweet Home</span>
+                    </div>
+
+                    <div className="search">
+                        <select>
+                            <option>All</option>
+                        </select>
+                        <input type="text" className="searchInput" />
+                        <SearchIcon className="searchIcon" />
+                    </div>
+
+                    <div className="header-nav">
+                        <Link to='/signin' className='header-link'>
+                            <div className="header-option">
+                                <span className="header-option1">Hello,</span>
+                                <span className="header-option2">{this.props.currentUser.name}</span>
+                            </div>
+                        </Link>
+
+                            <div className="header-option">
+                                <button className='header-option2-signout' type='submit' onClick={this.signedoutHome}>Sign Out</button>
+                            </div>
+
+                        <Link to='/checkout' className='header-link'>
+                            <div className="header-basket-homepage">
+                                <ShoppingCartIcon/>
+                                <span className="header-option2 basket-count">{Object.values(this.props.cart).length}</span>
+                            </div>
+                        </Link>
+
+                    </div>
+                </nav>
+            </div> 
+        )
+    };
+
+    guestHomepage() {
+        return (
+            <div>
+                <nav className="header">   
+                    <Link to="/">
+                        <img className="header-logo" 
+                            src="images/valyou-logo.png"
+                            alt="valyou-logo" 
+                            />
+                    </Link>
+
+                    <div className="header-option" style={{marginRight: "-10px"}}>
+                        <LocationOnIcon/>
+                    </div>
+
+                    <div className="header-option">
+                        <span className="header-option1">Deliver to</span>
+                        <span className="header-option2">Home Sweet Home</span>
+                    </div>
+
+                    <div className="search">
+                        <select>
+                            <option>All</option>
+                        </select>
+                        <input type="text" className="searchInput" />
+                        <SearchIcon className="searchIcon" />
+                    </div>
+
+                    <div className="header-nav">
+                            <div className="header-option">
+                                <span className="header-option1">Hello,</span>
+                                <span className="header-option2">Guest</span>
+                            </div>
+
+                        <Link to='/signin' className='header-link'>
+                            <div className="header-option">
+                                <button className='header-option2-signout'>Sign In</button>
+                            </div>
+                        </Link>
+
+                        <Link to='/checkout' className='header-link'>
+                            <div className="header-basket-homepage">
+                                <ShoppingCartIcon/>
+                                <span className="header-option2 basket-count">{Object.values(this.props.cart).length}</span>
+                            </div>
+                        </Link>
+                    </div>
+                </nav>
             </div>
-        </div>
-    );
+        );
+    };
+
+    render() {
+        const categories = [
+            "Mom & Pop",
+            "Farmers Market",
+            "Women-Owned",
+            "Veteran-Owned",
+            "Disability-Owned"
+        ]
+
+        return (
+            <div>
+                <span>
+                    {this.props.currentUser ? this.userHomepage() : this.guestHomepage()}
+                </span>
+
+                <div className="item-container">
+                    {categories && categories.map((category, idx) =>
+                        <p key={ idx }>{category}</p>
+                    )}
+                </div>
+            </div>
+        )
+    };
 };
 
-export default Header;
+export default withRouter(Header);
