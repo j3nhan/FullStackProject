6ac64712933ade8283a73cb75a5b879b77c6d9f5
j3nhan <j3nnynhan@gmail.com>
2021-09-19 12:06:09 -0700

    item show for add to cart is done

--- frontend/components/item/Item_show.jsx
+++ frontend/components/item/Item_show.jsx
@@ -88,11 +88,25 @@ class ItemShow extends React.Component {
                         </div>
 
                         <div className="add-to-cart-box">
-                            <div>One-time purchase</div>
-                            <div>Fastest delivery:</div>
-                            <div> { today } </div>
-                            <div>Order within 30 mins</div>
+                            <div className="pur" >One-time purchase</div>
+                            <div className="pur">Fastest delivery:</div>
+                            <div className="pur-date"> { today } </div>
                             <div className="in-stock">In Stock</div>
+                            <div className="qty-div">
+                                <label className='qty-label'>Qty:</label>
+                                <select name="qty" id="qty">
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                    <option value="6">6</option>
+                                    <option value="7">7</option>
+                                    <option value="8">8</option>
+                                    <option value="9">9</option>
+                                    <option value="10">10</option>
+                                </select>
+                            </div>
                             <button className="clickbutton" onClick={this.addToCart}>Add to Cart</button>
                         </div>
                     </div>
