501c791274d2cd19773e4a2bdae059f04c0d92b7
j3nhan <j3nnynhan@gmail.com>
2021-09-19 19:34:42 -0700

    resize row

--- frontend/components/item/Item_index.jsx
+++ frontend/components/item/Item_index.jsx
@@ -27,25 +27,38 @@ class ItemIndex extends React.Component {
         } else {
             return (
                 <div>
-                    <div className="item-index">
-                    <h1 className="all-items-head">Recommended For You</h1> 
-                    <div className="home">
-                        <div className="home-row">
-                                {this.props.items.slice(0, 5).map((item, idx) => <ItemCard key={idx} item={item} />)}
+                    <div className="item-index-cont">
+                        <h1 className="all-items-head">Recommended For You</h1> 
+                        
+                        <div className="home">
+                            <div className="catagory-name">Farmers Market</div>   
+                            <div className="home-row">
+                                    {this.props.items.slice(0, 5).map((item, idx) => <ItemCard key={idx} item={item} />)}
+                            </div>
+                            
+
+                            <div className="catagory-name">Women-Owned</div>
+                            <div className="home-row">
+                                    {this.props.items.slice(5, 10).map((item, idx) => <ItemCard key={idx} item={item} />)}
+                            </div>
+
+                            <div className="catagory-name">Veteran-Owned</div>
+                            <div className="home-row">
+                                    {this.props.items.slice(10, 15).map((item, idx) => <ItemCard key={idx} item={item} />)}
+                            </div>
+
+                            <div className="catagory-name">Mom & Pop</div>
+                            <div className="home-row">
+                                    {this.props.items.slice(15, 20).map((item, idx) => <ItemCard key={idx} item={item} />)}
+                            </div>
+
+                            
+                            <div className="catagory-name">Disability-Owned</div>
+                            <div className="home-row">
+                                    {this.props.items.slice(20, 25).map((item, idx) => <ItemCard key={idx} item={item} />)}
+                            </div>
                         </div>
-                        <div className="home-row">
-                                {this.props.items.slice(5, 10).map((item, idx) => <ItemCard key={idx} item={item} />)}
-                        </div>
-                        <div className="home-row">
-                                {this.props.items.slice(10, 15).map((item, idx) => <ItemCard key={idx} item={item} />)}
-                        </div>
-                        <div className="home-row">
-                                {this.props.items.slice(15, 20).map((item, idx) => <ItemCard key={idx} item={item} />)}
-                        </div>
-                        <div className="home-row">
-                                {this.props.items.slice(20, 25).map((item, idx) => <ItemCard key={idx} item={item} />)}
-                        </div>
-                    </div>
+
                     </div>
                 </div>
             )
