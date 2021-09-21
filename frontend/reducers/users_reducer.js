f97f82eef2fd4380732da3c8ef1c0be8114e04a8
j3nhan <j3nnynhan@gmail.com>
2021-09-21 11:32:04 -0700

    clear user info when signout

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -1,10 +1,14 @@
 import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
+import { SIGNOUT_CURRENT_USER } from "../actions/session_actions";
+
 
 const usersReducer = (state = {}, action) => {
     Object.freeze(state);
     switch(action.type) {
         case RECEIVE_CURRENT_USER:
             return Object.assign({}, state, { [action.user.id]: action.user });
+        case SIGNOUT_CURRENT_USER:
+            return {};
         default:
             return state;
     }
