805f97bf04b6d6876b4c5f49d13dfde78125265c
j3nhan <j3nnynhan@gmail.com>
2021-09-21 11:30:48 -0700

    add signout user to clear state

--- frontend/reducers/cart_items_reducer.js
+++ frontend/reducers/cart_items_reducer.js
@@ -1,4 +1,5 @@
 import { RECEIVE_CART_ITEMS, RECEIVE_CART_ITEM, REMOVE_CART_ITEM, CLEAR_CART } from "../actions/cart_items_actions";
+import { SIGNOUT_CURRENT_USER } from "../actions/session_actions";
 
 const cartItemsReducer = (state = [], action) => {
     Object.freeze(state);
@@ -15,6 +16,8 @@ const cartItemsReducer = (state = [], action) => {
         case CLEAR_CART:
             delete nextState[action.cartItems];
             return nextState;
+        case SIGNOUT_CURRENT_USER:
+            return [];
         default: 
             return state;
     }
