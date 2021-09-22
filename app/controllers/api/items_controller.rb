c0a92cecde4188984f771f769fe5d3f2bd1f57e9
j3nhan <j3nnynhan@gmail.com>
2021-09-22 15:13:21 -0700

    work on search features

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -8,5 +8,10 @@ class Api::ItemsController < ApplicationController
         @item = Item.find(params[:id])
         render :show
     end
+
+    def search
+        @items = Item.search_by(params[:search])
+        render :search_results
+    end
     
 end
