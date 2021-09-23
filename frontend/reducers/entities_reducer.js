cb7695c21b468aa300340959e3b1496331055a80
j3nhan <j3nnynhan@gmail.com>
2021-09-23 15:07:10 -0700

    include search function

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -3,12 +3,14 @@ import usersReducer from './users_reducer';
 import itemsReducer from './items_reducer';
 import cartItemsReducer from './cart_items_reducer';
 import reviewsReducer from './reviews_reducer';
+import searchReducer from './items_search_reducer';
 
 const entitiesReducer = combineReducers({
     users: usersReducer,
     items: itemsReducer,
     cartItems: cartItemsReducer,
     reviews: reviewsReducer,
+    search: searchReducer
 })
 
 export default entitiesReducer;
