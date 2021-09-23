99ac292bf0aca9e03e0ab8db2804e2c55e5c7bda
j3nhan <j3nnynhan@gmail.com>
2021-09-23 15:03:58 -0700

    include search route

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,16 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: {format: :json} do
     resource :session, only: [:create, :destroy, :show]
+    
     resources :users, only: [:create] 
+
+    get "/items/search", to: "items#search"
     resources :items, only: [:index, :show] do 
       resources :reviews, only: [:index, :show, :create]
     end
+
     resources :reviews, only: [:destroy]
+
     resources :cart_items, except: [:new, :edit] do
       delete "clear", on: :collection
     end
