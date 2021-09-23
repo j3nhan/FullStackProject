4cd718d4998988b3cbd77de8cea2c4a16f045463
j3nhan <j3nnynhan@gmail.com>
2021-09-23 15:03:40 -0700

    include search def

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,9 @@ class Item < ApplicationRecord
     has_one_attached :photo
     has_many :reviews, foreign_key: :item_id
     has_many :cart_items, foreign_key: :item_id
+
+    def self.search_by(query)
+        self.where("item_name ILIKE ?", "%#{query}%")
+            .or(self.where("description ILIKE ?", "%#{query}%"))
+    end
 end
