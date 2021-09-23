3eda9942a450091ebfbcccf233f31469dc1301fc
j3nhan <j3nnynhan@gmail.com>
2021-09-23 15:09:08 -0700

    search component is made

---
+++ frontend/components/search/nav_search.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import SearchIcon from '@material-ui/icons/Search';
+
+class NavSearch extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { keyword: '' }
+
+        this.searchTerm = this.searchTerm.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        this.props.fetchItemsSearch(this.state.keyword)
+        .then(() => { this.props.history.push('/search')})
+    }
+
+    searchTerm(e) {
+        e.preventDefault();
+        this.setState({ keyword: e.currentTarget.value })
+    }
+
+    render() {
+        return (
+            <form className="search" onSubmit={this.handleSubmit}>
+                <select>
+                    <option>All</option>
+                </select>
+                <input type="text" className="searchInput" value={this.state.keyword} onChange={this.searchTerm} />
+                <SearchIcon className="searchIcon" onClick={this.handleSubmit} />
+            </form>
+        )
+    }
+
+}
+
+export default withRouter(NavSearch);
